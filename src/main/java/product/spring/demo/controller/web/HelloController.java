b4b379bb7802f2a2284dcd8f462be363d09d2ef2
huypn <huypn@Laptop086.nttdata.com.vn>
2018-10-26 18:52:31 +0700

    add new setView

--- src/main/java/product/spring/demo/controller/web/HelloController.java
+++ src/main/java/product/spring/demo/controller/web/HelloController.java
@@ -1,16 +1,17 @@
 package product.spring.demo.controller.web;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.support.HttpRequestHandlerServlet;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 import product.spring.demo.model.Product;
 
@@ -24,18 +25,41 @@ public class HelloController {
 
 	@RequestMapping(value = "/product")
 	public ModelAndView products() {
-		ModelAndView m = new ModelAndView("product");
-		m.addObject("msg", "Spring Hello");
-		List<Product> productList = new ArrayList<>();
-
-		Product p1 = new Product();
-		p1.setId(1);
-		p1.setName("Product1");
-		p1.setPrice("3000$");
-		productList.add(p1);
-
-		m.addObject("msg", "Spring Hello");
-		m.addObject("allProduct", productList);
+		ModelAndView m = new ModelAndView();
+		m.setView(new View() {
+			@Override
+			public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response)
+					throws Exception {
+				// TODO Auto-generated method stub
+//				request.setAttribute("msg", "Spring Hello");
+				List<Product> productList = new ArrayList<>();
+		
+				Product p1 = new Product();
+				p1.setId(1);
+				p1.setName("Product1");
+				p1.setPrice("3000$");
+				productList.add(p1);
+		
+				request.setAttribute("msg", "Spring Hello");
+				request.setAttribute("allProduct", productList);
+				
+				request.getRequestDispatcher("\\WEB-INF\\views\\product.jsp").forward(request, response);
+			}
+		});
+		
+//		m.setViewName("product");
+		
+//		m.addObject("msg", "Spring Hello");
+//		List<Product> productList = new ArrayList<>();
+//
+//		Product p1 = new Product();
+//		p1.setId(1);
+//		p1.setName("Product1");
+//		p1.setPrice("3000$");
+//		productList.add(p1);
+//
+//		m.addObject("msg", "Spring Hello");
+//		m.addObject("allProduct", productList);
 		return m;
 	}
 
@@ -44,5 +68,7 @@ public class HelloController {
 		request.setAttribute("title", "Hello Spring MVC");
 		return "hello";
 	}
+	
+	
 
 }
